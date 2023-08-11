34f32dca2f057d0f84232c614f5ff95b10d93fda
yayat_99 <rahmat999ht@gmail.com>
2023-08-11 18:54:54 +0800

    update Admin Model

--- lib/app/core/models/admin.dart
+++ lib/app/core/models/admin.dart
@@ -1,13 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
 class AdminModel {
   final String? id;
   final String? userName;
   final String? pass;
+  final String? nama;
+  final String? jkl;
   AdminModel({
     this.id,
     this.userName,
     this.pass,
+    this.nama,
+    this.jkl,
   });
 
   Map<String, dynamic> toMap() {
@@ -15,17 +21,23 @@ class AdminModel {
       'id': id,
       'userName': userName,
       'pass': pass,
+      'nama': nama,
+      'jkl': jkl,
     };
   }
 
-  factory AdminModel.fromMapById(String id, Map<String, dynamic> map) {
+  factory AdminModel.fromMapById(
+    String id,
+    Map<String, dynamic> map,
+  ) {
     return AdminModel(
       id: id,
       userName: map['userName'] != null ? map['userName'] as String : null,
       pass: map['pass'] != null ? map['pass'] as String : null,
+      nama: map['nama'] != null ? map['nama'] as String : null,
+      jkl: map['jkl'] != null ? map['jkl'] as String : null,
     );
   }
-
   factory AdminModel.fromDocumentSnapshot(
     DocumentSnapshot<Map<String, dynamic>> data,
   ) =>
